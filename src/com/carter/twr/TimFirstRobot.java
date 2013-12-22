6b6059a342ec10bc9d0e4ff1762441488e5931ce
tim carter <twrcarter@hotmail.co.uk>
2013-12-22 16:49:40 +0000

    tica: t70 basher coming atcha :)

--- src/com/carter/twr/TimFirstRobot.java
+++ src/com/carter/twr/TimFirstRobot.java
@@ -1,46 +1,41 @@
 package com.carter.twr;
-import robocode.*;
+
+import robocode.HitWallEvent;
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
 
 /**
  * TimFirstRobot - a robot by Timboslice Carter
  */
 public class TimFirstRobot extends Robot {
 
-	public void run() {
-		setAdjustRadarForGunTurn(true);
-		
-		do {
-        	turnRadarRight(360);
-    	} while (true);
-	}
+    public void run() {
+        setAdjustRadarForGunTurn(true);
 
-	public void onScannedRobot(ScannedRobotEvent e) {
-		double targetBearing = e.getBearing();
-		double turnGunAmt = (getHeading() + e.getBearing()) - getGunHeading();
-		if (turnGunAmt >= 0) {
-			turnGunRight(turnGunAmt);
-		} else {
-			turnGunLeft(-turnGunAmt);	
-		}		
-		
-		if (e.getDistance() < 140) {
-			if (getGunHeat() == 0) {
-				fire(2.5);		
-			}
-		} else {
-			if (targetBearing >= 0) {
-				turnRight(targetBearing);
-			} else {	
-				turnLeft(targetBearing);
-			}
-			ahead(e.getDistance() - 120);
-		}
-		scan();
-	}
+        do {
+            turnRadarRight(360);
+        } while (true);
+    }
 
-	public void onHitWall(HitWallEvent e) {
-		back(10);
-		turnLeft(180);
-		ahead(100);
-	}	
+    public void onScannedRobot(ScannedRobotEvent e) {
+        double targetBearing = e.getBearing();
+        double turnGunAmt = (getHeading() + e.getBearing()) - getGunHeading();
+        turnGunRight(turnGunAmt);
+
+        if (e.getDistance() < 140) {
+            if (getGunHeat() == 0) {
+                fire(2.5);
+            }
+        } else {
+            turnRight(targetBearing);
+            ahead(e.getDistance() - 120);
+        }
+        scan();
+    }
+
+    public void onHitWall(HitWallEvent e) {
+        back(10);
+        turnLeft(180);
+        ahead(100);
+    }
 }
